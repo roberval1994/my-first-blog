47581ec77ae625527d827097d988dc122679aa97
Roberval <roberval1994@live.com>
2020-07-13 18:47:57 -0300

    My Django app, First Commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,22 @@
+from datetime import timezone
+
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from mysite import settings
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
